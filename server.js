eb207f52c81396fa237d233384764b9e94c3a896
jonesmattc <jones.matt.c@gmail.com>
2012-09-04 22:55:39 -0500

    fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ function start(route, handle) {
     route(handle, pathname, response, request);
   }
 
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(process.env.PORT || 5000);
   console.log("Server has started.");
 }
 
